fdd99c07b8791e83cc67d5edab9bdd52c7ed32c8
feoranis26 <alpdoganer26@gmail.com>
2026-01-21 17:26:41 -0600

    subsystems: refactor ExposureControl dummy

--- src/chamber_ctl/subsystems/exposure_controller.py
+++ src/chamber_ctl/subsystems/exposure_controller.py
@@ -1,7 +1,7 @@
+import multiprocessing
 import time
 import uuid
 import sys
-from multiprocessing import Event
 
 import ipi_ecs.dds.subsystem as subsystem
 import ipi_ecs.dds.types as types
@@ -11,31 +11,65 @@ import ipi_ecs.core.tcp as tcp
 
 from ipi_ecs.logging.client import LogClient
 
-def main(stop_event: Event):
-    c_uuid = uuid.uuid4()
+from chamber_ctl.subsystems.uuids import UUID_EXPOSURE_CONTROLLER
 
-    sock = tcp.TCPClientSocket()
+class ExposureController:
+    def __init__(self):
+        self.__run = True
 
-    sock.connect(("127.0.0.1", 11751))
-    sock.start()
+        c_uuid = uuid.uuid4()
 
-    logger = LogClient(sock, origin_uuid=c_uuid)
+        self.__logger_sock = tcp.TCPClientSocket()
 
-    m_client = client.DDSClient(c_uuid, logger=logger)
-    s = None
+        self.__logger_sock.connect(("127.0.0.1", 11751))
+        self.__logger_sock.start()
 
-    def reg_s():
-        s = m_client.register_subsystem("Exposure Controller", uuid.uuid3(uuid.NAMESPACE_OID, "1"))
+        self.__logger = LogClient(self.__logger_sock, origin_uuid=c_uuid)
 
-    m_client.when_ready().then(reg_s)
-    time.sleep(1)
+        self.__did_config = False
+        self.__subsystem = None
+
+        def _on_ready():
+            if self.__did_config:
+                return
+            
+            self.__did_config = True
+            sh = self.__client.register_subsystem("Exposure Controller", UUID_EXPOSURE_CONTROLLER)
+
+            self.__on_got_subsystem(sh)
+
+        #print("Registering subsystem...")
+        self.__client = client.DDSClient(c_uuid, logger=self.__logger)
+        self.__client.when_ready().then(_on_ready)
+        
+
+    def __on_got_subsystem(self, handle: client._RegisteredSubsystemHandle):
+        # state has to be named state as it will get passed as a KW argument, stop complaining Pylint!!
+        def fail(state, reason): #pylint: disable=unused-argument
+            print(f"Failed: {reason}")
+            self.__run = False
+
+        self.__subsystem = handle
+
+    def ok(self):
+        return self.__run and self.__client.ok()
+    
+    def close(self):
+        self.__client.close()
+        self.__logger_sock.close()
+
+        self.__run = False
+
+def main(stop_event: "multiprocessing.Event"):
+    m_exposure_controller = ExposureController()
 
     try:
-        while m_client.ok() and not stop_event.is_set():
+        while m_exposure_controller.ok() and not (stop_event is not None and stop_event.is_set()):
             time.sleep(1)
     except KeyboardInterrupt:
         pass
     finally:
-        m_client.close()
-        time.sleep(0.1)
-        sock.close()
+        m_exposure_controller.close()
+
+if __name__ == "__main__":
+    main(None)
