c5617ba0661b42d22ba92deaa34b55413737a7c2
feoranis26 <alpdoganer26@gmail.com>
2026-02-04 11:12:13 -0600

    add experiment controller-based exposure controller instead of dummy

--- src/chamber_ctl/subsystems/exposure_controller.py
+++ src/chamber_ctl/subsystems/exposure_controller.py
@@ -1,92 +1,64 @@
 import multiprocessing
+import os
+import pickle
+import queue
 import time
 import uuid
-import sys
+import mt_events
+import segment_bytes
 
+from enum import Enum
+
+from ipi_ecs.core import daemon
 import ipi_ecs.dds.subsystem as subsystem
 import ipi_ecs.dds.types as types
 import ipi_ecs.dds.client as client
 import ipi_ecs.dds.magics as magics
 import ipi_ecs.core.tcp as tcp
+from ipi_ecs.dds.magics import *
 
 from ipi_ecs.logging.client import LogClient
+from ipi_ecs.db.db_library import Library
 
+from ipi_ecs.subsystems.experiment_controller import ExperimentController, RunSettings
 from chamber_ctl.subsystems import uuids
 
-class ExposureSettings:
-    target_dose = 0.0
-    target_time = 0.0
+class ExposureSettings(RunSettings):
+    def __init__(self, target_time: float = 0.0, target_dose: float = 0.0):
+        super().__init__()
+        self.data["target_time"] = target_time
+        self.data["target_dose"] = target_dose
 
-    def encode(self):
-        return pickle.dumps(self)
+    def get_target_time(self) -> float:
+        return self.data.get("target_time", 0.0)
+    
+    def get_target_dose(self) -> float:
+        return self.data.get("target_dose", 0.0)
     
     @staticmethod
-    def decode(b_data: bytes):
-        return pickle.loads(b_data)
-    
-class ExposureState:
-    def __init__(self, exposure_settings: ExposureSettings):
-        self.exposure_settings = exposure_settings
-        self.__uuid = uuid.uuid4()
+    def decode(data: bytes) -> "ExposureSettings":
+        obj = pickle.loads(data)
+        exp_settings = ExposureSettings()
+        exp_settings.data = obj.data
 
+        assert obj.data.get("target_time") is not None, "Decoded ExposureSettings missing target_time"
+        assert obj.data.get("target_dose") is not None, "Decoded ExposureSettings missing target_dose"
 
-class ExposureController:
-    def __init__(self):
-        self.__run = True
-
-        c_uuid = uuid.uuid4()
-
-        self.__logger_sock = tcp.TCPClientSocket()
-
-        self.__logger_sock.connect(("127.0.0.1", 11751))
-        self.__logger_sock.start()
-
-        self.__logger = LogClient(self.__logger_sock, origin_uuid=c_uuid)
-
-        self.__did_config = False
-        self.__subsystem = None
-
-        def _on_ready():
-            if self.__did_config:
-                return
-            
-            self.__did_config = True
-            sh = self.__client.register_subsystem("Exposure Controller", UUID_EXPOSURE_CONTROLLER)
-
-            self.__on_got_subsystem(sh)
-
-        #print("Registering subsystem...")
-        self.__client = client.DDSClient(c_uuid, logger=self.__logger)
-        self.__client.when_ready().then(_on_ready)
-        
-
-    def __on_got_subsystem(self, handle: client._RegisteredSubsystemHandle):
-        # state has to be named state as it will get passed as a KW argument, stop complaining Pylint!!
-        def fail(state, reason): #pylint: disable=unused-argument
-            print(f"Failed: {reason}")
-            self.__run = False
-
-        self.__subsystem = handle
-
-    def ok(self):
-        return self.__run and self.__client.ok()
-    
-    def close(self):
-        self.__client.close()
-        self.__logger_sock.close()
-
-        self.__run = False
+        return exp_settings
 
 def main(stop_event: "multiprocessing.Event"):
-    m_exposure_controller = ExposureController()
+    __SAVE_PATH = os.path.join(os.environ["EUVL_PATH"], "datasets")
+    m_run_controller = ExperimentController("ExposureController", uuids.UUID_EXPOSURE_CONTROLLER, "exposure", __SAVE_PATH)
+    m_run_controller.add_required_subsystem(uuids.UUID_TARGET_CONTROLLER)
+    m_run_controller.register_experiment_settings_type(ExposureSettings)
 
     try:
-        while m_exposure_controller.ok() and not (stop_event is not None and stop_event.is_set()):
+        while m_run_controller.ok() and not (stop_event is not None and stop_event.is_set()):
             time.sleep(1)
     except KeyboardInterrupt:
         pass
     finally:
-        m_exposure_controller.close()
+        m_run_controller.close()
 
 if __name__ == "__main__":
     main(None)
