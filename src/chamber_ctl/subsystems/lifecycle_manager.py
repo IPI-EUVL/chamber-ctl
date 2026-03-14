dbcf26791cebd9132a45f976d9a5ec9072ff4658
Alp Doganer <doganer2@illinois.edu>
2026-03-13 19:03:12 -0500

    add subsystems to lifecycle manager

--- src/chamber_ctl/subsystems/lifecycle_manager.py
+++ src/chamber_ctl/subsystems/lifecycle_manager.py
@@ -4,13 +4,15 @@ import time
 
 from ipi_ecs.subsystems.lifecycle_manager import LifecycleManager
 
-from chamber_ctl.subsystems import exposure_controller
+from chamber_ctl.subsystems import exposure_controller, target_controller, oscilloscope
 import chamber_ctl.subsystems.uuids as uuids
 
 
 def main(stop_event: "multiprocessing.Event"):
     m_client = LifecycleManager(uuids.UUID_LIFECYCLE_MANAGER)
     m_client.add_subsystem(uuids.UUID_EXPOSURE_CONTROLLER, exposure_controller.main)
+    m_client.add_subsystem(uuids.UUID_TARGET_CONTROLLER, target_controller.main)
+    m_client.add_subsystem(uuids.UUID_OSCILLOSCOPE_CONTROLLER, oscilloscope.main)
 
     try:
         while m_client.ok() and not (stop_event is not None and stop_event.is_set()):
