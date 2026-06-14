22ac4e1b00c5d93d7d5ebffeb6f6c10d693877ad
Alp Doganer <doganer2@illinois.edu>
2026-06-15 01:43:41 +0300

    use remote id for remote branch

--- src/chamber_ctl/__init__.py
+++ src/chamber_ctl/__init__.py
@@ -1,5 +1,6 @@
 import os
 
-
-ECS_IP = os.environ.get("ECS_HOST", "127.0.0.1")
+ECS_IP = "10.194.210.21" #os.environ.get("ECS_HOST", "127.0.0.1")
 ECS_PORT = int(os.environ.get("ECS_PORT", 11750))
+
+print(f"ECS_IP: {ECS_IP}, ECS_PORT: {ECS_PORT}")
